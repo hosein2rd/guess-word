7829bf8b4c593045c30daf1feee3918804612b33
hossein <mdoroud97@gmail.com>
2022-11-13 20:09:12 +0330

    feat(Button.tsx): add button

---
+++ src/components/Button.tsx
@@ -0,0 +1,33 @@
+import {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  AnchorHTMLAttributes,
+} from "react"
+
+type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  DetailedHTMLProps<
+    AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  > & {
+    fill?: boolean
+    color?: string
+    backgroundColor?: string
+  }
+
+export default function Button({
+  className = "",
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={`${className} bg-blue-400 hover:bg-blue-500 disabled:bg-gray-400 shadow-2xl transition-all text-white py-1 px-3 rounded-md `}
+    >
+      {children}
+    </button>
+  )
+}
